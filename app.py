b8e401a917dd72305cd33fe4956c221d09958d8f
DuncanCSt <dcameronsteinke@gmail.com>
2025-06-25 22:48:41 +0000

    Add data loading functionality and initial database setup

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import sqlite3
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    conn = sqlite3.connect("rfi.db")
+    df = pd.read_sql_query("SELECT * FROM rfi_table", conn)
+    conn.close()
+    return df
 
 st.title("My Streamlit App")
-st.write("Hello, Streamlit!")
+df = load_data()
+st.write(df.head())
