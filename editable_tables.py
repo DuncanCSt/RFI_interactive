37974938cb32804658792014ae04b74f41b6f6db
DuncanCSt <dcameronsteinke@gmail.com>
2025-06-26 12:09:41 -0700

    Refactor app logic into separate modules

---
+++ editable_tables.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from st_aggrid import (
+    AgGrid,
+    GridOptionsBuilder,
+    GridUpdateMode,
+    DataReturnMode,
+    JsCode,
+)
+
+
+def editable_transitions_table(df: pd.DataFrame) -> pd.DataFrame:
+    """Display an editable AG Grid table for ``df`` and return the edited DataFrame."""
+    row_style = JsCode(
+        """
+function(params) {
+  if (params.data.score === 'fully clean') {
+    return { background: 'lightgreen' };
+  } else if (params.data.score === 'partially clean') {
+    return { background: 'lightyellow' };
+  } else if (params.data.score === 'not clean') {
+    return { background: 'lightcoral' };
+  }
+  return {};
+}
+"""
+    )
+
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_column(
+        "score",
+        editable=True,
+        cellEditor="agSelectCellEditor",
+        cellEditorParams={"values": ["fully clean", "partially clean", "not clean"]},
+    )
+    gb.configure_grid_options(getRowStyle=row_style)
+    grid_options = gb.build()
+
+    grid_response = AgGrid(
+        df,
+        gridOptions=grid_options,
+        update_mode=GridUpdateMode.VALUE_CHANGED,
+        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+        allow_unsafe_jscode=True,
+    )
+    return grid_response["data"]
