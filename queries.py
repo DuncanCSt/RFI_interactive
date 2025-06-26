e241cb681b327cfe4de6d5a49e5307c776a8785a
Duncan cs <87150626+duncancs-homex@users.noreply.github.com>
2025-06-26 10:26:35 -0700

    Refactor data loading functions for RFI, summary, and transitions tables

--- queries.py
+++ queries.py
@@ -2,9 +2,26 @@ import sqlite3
 import pandas as pd
 import streamlit as st
 
+## RFI Table
 @st.cache_data
 def load_data():
     conn = sqlite3.connect("rfi.db")
     df = pd.read_sql_query("SELECT * FROM rfi_table", conn)
+    conn.close()
+    return df
+
+## Summary Table
+@st.cache_data
+def load_summary_data():
+    conn = sqlite3.connect("rfi.db")
+    df = pd.read_sql_query("SELECT * FROM summary_table", conn)
+    conn.close()
+    return df
+
+## Transitions Table
+@st.cache_data
+def load_transitions_data():
+    conn = sqlite3.connect("rfi.db")
+    df = pd.read_sql_query("SELECT * FROM transitions_table", conn)
     conn.close()
     return df
